606dbaadd3a265d82316475cd50b2059c6c9000f
monishilango16 <monishniveth@yahoo.in>
2024-05-13 18:03:56 -0500

    UPDATING TESTCASE

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("render", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/WELCOME TO PORTFOLIO CRAFT/);
   expect(linkElement).toBeInTheDocument();
 });
